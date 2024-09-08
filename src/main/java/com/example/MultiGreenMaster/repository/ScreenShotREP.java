00f48de877862133282352600e37f144a803ae75
heon <chamchi5055@gmail.com>
2024-09-08 21:18:26 +0900

    스크린샷(screenshot)관련 파일 삭제
    구버젼 udp 테스트 클래스 삭제

--- src/main/java/com/example/MultiGreenMaster/repository/ScreenShotREP.java
+++
@@ -1,7 +0,0 @@
-package com.example.MultiGreenMaster.repository;
-
-import com.example.MultiGreenMaster.entity.ScreenShotENT;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ScreenShotREP extends JpaRepository<ScreenShotENT, Long> {
-}
