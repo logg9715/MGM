66fd767b3ff7ad4bade753b0c5c3a12cae77d4a5
heon <chamchi5055@gmail.com>
2024-09-08 14:40:25 +0900

    DiaryBoard 상세보기 수정 현재 Diary 에서 id 1인 게시글이 안나오는 버그가있음

--- src/main/java/com/example/MultiGreenMaster/dto/DiaryBoardFRM.java
+++ src/main/java/com/example/MultiGreenMaster/dto/DiaryBoardFRM.java
@@ -23,19 +23,20 @@ public class DiaryBoardFRM {
     private List<MultipartFile> pictures; // 사진 리스트 (파일 형식)
     private List<String> pictureBase64List; // 사진 리스트 (Base64 인코딩 형식)
     private LocalDateTime regdate; // 작성 시간
-    private Long isPublic; // 공개 여부
+    private Long isPublic; // 0 = 비공개, 1 = 전체 공개, 2 = 친구에게만 공개
 
     // 기본 생성자 이외에 특정 필드들을 초기화하는 생성자
-    public DiaryBoardFRM(Long id, UserENT user, String title, String content, List<String> pictureBase64List, LocalDateTime regdate, boolean isPublic) {
+    public DiaryBoardFRM(Long id, UserENT user, String title, String content, List<String> pictureBase64List, LocalDateTime regdate, Long isPublic) {
         this.id = id;
         this.user = user;
         this.title = title;
         this.content = content;
         this.pictureBase64List = pictureBase64List;
         this.regdate = regdate;
+        this.isPublic = isPublic;
     }
 
-    // 엔티티로 변환하는 메서드, pictureBytesList는 List<byte[]> 형식의 이미지 데이터를 받음
+    // 엔티티로 변환하는 메서드
     public DiaryBoardENT toEntity(List<byte[]> pictureBytesList) {
         return DiaryBoardENT.builder()
                 .id(this.id)
