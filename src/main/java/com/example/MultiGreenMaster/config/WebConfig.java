3c8d60dea690fab7af9abeb72a1b37ceab3c31a5
heon <chamchi5055@gmail.com>
2024-09-09 16:07:47 +0900

    /config/Webconfig클래스 잘못된 프로젝트명 수정

--- src/main/java/com/example/MultiGreenMaster/config/WebConfig.java
+++ src/main/java/com/example/MultiGreenMaster/config/WebConfig.java
@@ -1,4 +1,4 @@
-package com.example.Flower.config;
+package com.example.MultiGreenMaster.config;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
