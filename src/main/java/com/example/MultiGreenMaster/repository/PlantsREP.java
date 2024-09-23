8b5b221aead046269be985dcdcdec8bd21f586b1
heon <chamchi5055@gmail.com>
2024-09-23 14:21:06 +0900

    식물 관련된 entity+관련 클래스 삭제
    제목과 같다.

--- src/main/java/com/example/MultiGreenMaster/repository/PlantsREP.java
+++
@@ -1,8 +0,0 @@
-package com.example.MultiGreenMaster.repository;
-
-import com.example.MultiGreenMaster.entity.PlantsENT;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface PlantsREP extends JpaRepository<PlantsENT, Long> {
-
-}
--- src/main/java/com/example/MultiGreenMaster/repository/UsersPlantREP.java
+++
@@ -1,8 +0,0 @@
-package com.example.MultiGreenMaster.repository;
-
-import com.example.MultiGreenMaster.entity.User_PlantENT;
-import org.springframework.data.repository.CrudRepository;
-
-public interface UsersPlantREP extends CrudRepository<User_PlantENT, Long> {
-
-}
